260989bf4a0d8217e3ff086bf6f196f48f0fdf27
osohare <hector.rodriguez@kebir.com.mx>
2015-03-03 20:01:44 -0800

    Initial checkin for the newtech solutions MVC based

---
+++ FabrikamSolutions/Fabrikam/Fabrikam.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fabrikam.MVC.MvcApplication" Language="C#" %>
---
+++ FabrikamSolutions/Fabrikam/Fabrikam.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fabrikam.MVC.Startup))]
+namespace Fabrikam.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
