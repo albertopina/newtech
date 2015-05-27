5631efd0662a82ddc29f0605acc6f5181a0e1245
albertopina <alberto.pina@knova.com.mx>
2015-05-26 22:12:26 -0500

    Servicio Clientes

--- NewTech/NewTech.Console.Test/Module1.vb
+++ NewTech/NewTech.Console.Test/Module1.vb
@@ -1,19 +1,65 @@
-﻿Module Module1
+﻿Imports NewTech.Services
+Imports NewTech.Library.Database
+
+Module Module1
 
     Sub Main()
         AppDomain.CurrentDomain.SetData("DataDirectory", "C:\Users\" & Environment.UserName & "\Source\DB")
 
-        Dim myContext As New NewTech.Library.Database.NewtechContext
+        'Dim myContext As New NewTech.Library.Database.NewtechContext
 
-        Dim myCliente As New NewTech.Library.Database.Client
-        With myCliente
-            .Nombre = "Knova SA de CV"
-        End With
+        'Dim myCliente As New NewTech.Library.Database.Client
+        'With myCliente
+        '    .Nombre = "Knova SA de CV"
+        'End With
 
-        myContext.Clientes.Add(myCliente)
-        myContext.SaveChanges()
-        myContext.Dispose()
+        'myContext.Clients.Add(myCliente)
+        'myContext.SaveChanges()
+        'myContext.Dispose()
 
+        
+
+        Using myClientService As New ClientService
+            'Try
+            '    Dim myClient As New Client()
+            '    myClient.ID = 3
+            '    myClientService.Delete(myClient)
+            '    myClientService.Save()
+            '    System.Console.WriteLine("Ya te borro pinche Knova puto!")
+            'Catch ex As Exception
+            '    System.Console.WriteLine(ex.Message)
+            'End Try
+
+            'Try
+            '    Dim newClient As New Client()
+            '    'newClient.ID = 3
+            '    newClient.Nombre = "El Oso es baboso"
+            '    myClientService.Insert(newClient)
+            '    myClientService.Save()
+            'Catch ex As Exception
+            '    System.Console.WriteLine(ex.Message)
+            'End Try
+
+            Try
+                Dim myClient As New Client
+                myClient.ID = 6
+                myClient.Nombre = "No es tan baboso"
+                myClientService.Update(myClient)
+                myClientService.Save()
+            Catch ex As Exception
+                System.Console.WriteLine(ex.Message)
+            End Try
+
+
+            Dim myList As List(Of Client) = myClientService.GetClients()
+
+            For Each mClient As Client In myList
+                System.Console.WriteLine(mClient.ID & " - " & mClient.Nombre)
+            Next
+
+            System.Console.ReadKey()
+
+        End Using
     End Sub
 
 End Module
--- NewTech/NewTech.Library.Database/Context/NewtechContext.vb
+++ NewTech/NewTech.Library.Database/Context/NewtechContext.vb
@@ -3,8 +3,8 @@
 Public Class NewtechContext
     Inherits DbContext
 
-    Public Property Clientes As DbSet(Of Client)
-    Public Property Actividades As DbSet(Of Activity)
+    Public Property Clients As DbSet(Of Client)
+    Public Property Activities As DbSet(Of Activity)
 
     Public Sub New()
         MyBase.New("Name=NewtechConnection")
